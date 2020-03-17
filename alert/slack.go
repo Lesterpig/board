391429884cec7ca21c3329a536f30cc45b615c7c
Frederik Mogensen <fmo@trifork.com>
2020-03-17 08:59:39 +0100

    Cleanup

--- alert/slack.go
+++ alert/slack.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -12,64 +13,77 @@ import (
 
 // Slack alert container.
 type Slack struct {
-	client     *http.Client
-	webhookURL string
-	channel    string
+	client      *http.Client
+	webhookURL  string
+	channel     string
+	httpTimeout time.Duration
 }
 
 // NewSlack returns a Slack alerter from the webhookURL
 func NewSlack(webhookURL, channel string) *Slack {
 	return &Slack{
-		client:     &http.Client{},
-		webhookURL: webhookURL,
-		channel:    channel,
+		client:      &http.Client{},
+		webhookURL:  webhookURL,
+		channel:     channel,
+		httpTimeout: time.Duration(10) * time.Second,
 	}
 }
 
 // Alert sends a pushbullet note to the owner of the provided token.
 func (p *Slack) Alert(status probe.Status, category, title, message, link, date string) {
-
 	color := "#ff0000"
 	if status == probe.StatusOK {
 		color = "#00ff00"
 	}
-	rocketChat := SlackPostMessage{Channel: p.channel, Alias: category}
-	rocketChat.Attachments = []Attachment{
-		Attachment{
-			Color: color,
-			Fields: []AttachmentField{
-				{
-					Short: false,
-					Title: fmt.Sprintf("%s is %s", title, status),
-					Value: fmt.Sprintf("[%s](%s) - Response: %s at (%s)", title, link, message, date),
+
+	slackMessage := SlackPostMessage{
+		Channel: p.channel,
+		Alias:   category,
+		Attachments: []Attachment{
+			{
+				Color: color,
+				Fields: []AttachmentField{
+					{
+						Short: false,
+						Title: fmt.Sprintf("%s is %s", title, status),
+						Value: fmt.Sprintf("[%s](%s) - Response: %s at (%s)", title, link, message, date),
+					},
 				},
 			},
 		},
 	}
 
-	slackBody, _ := json.Marshal(rocketChat)
+	slackBody, _ := json.Marshal(slackMessage)
 	req, err := http.NewRequest(http.MethodPost, p.webhookURL, bytes.NewBuffer(slackBody))
+
 	if err != nil {
 		return
 	}
+
 	req.Header.Add("Content-Type", "application/json")
 
-	client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: p.httpTimeout}
+
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Errorf("Error sending request to  Slack: %s ", err)
 		return
 	}
+	defer resp.Body.Close()
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
-	if buf.String() != "{\"success\":true}" {
-		log.Errorf("Non-ok response returned from Slack: %s ", buf.String())
-		return // errors.New("Non-ok response returned from Slack")
+	bytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Errorf("Error response from Slack: %s ", err)
+		return
 	}
-	log.Info("Alert sendt")
 
-	return
+	if string(bytes) != "{\"success\":true}" {
+		log.Errorf("Non-ok response returned from Slack: %s ", string(bytes))
+		return
+	}
+
+	log.Info("Alert sendt")
 }
 
 // All models with inspiration from https://github.com/RocketChat/Rocket.Chat.Go.SDK/blob/master/models/message.go
